9fac68aa012097397ebd5e30147ed63d68c1f363
Eason Li <lijinghua@odc.cmbchina.cn>
2019-07-01 20:08:53 +0800

    complete practise six

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+    
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    
+    public Integer getKlass() {
+        return klass;
+    }
+    
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+    
+    @Override
+    public String introduce() {
+        String introduceStr = super.introduce() + " I am a Teacher. I teach ";
+        if (this.klass == null) {
+            introduceStr += "No Class.";
+        } else {
+            introduceStr += "Class "+this.klass + ".";
+        }
+        return introduceStr;
+    }
 }
